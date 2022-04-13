307da3b5bf28362a0b7e6581decc8c0893904733
R@z@R <84044032+razorads@users.noreply.github.com>
2022-04-14 02:38:18 +0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -20,10 +20,10 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : R@z@R"
+print "You Tube : https://www.youtube.com/c/razor"
+print "github   : https://github.com/razorads"
+print "suluyaylak_00 : razor"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
